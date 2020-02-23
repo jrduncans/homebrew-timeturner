13025aa6cb7940c037cb8d2b31022d451a89fc3b
Stephen Duncan <jrduncans@users.noreply.github.com>
2020-02-23 13:47:38 -0800

    Initial Homebrew formula for timeturner

---
+++ HomebrewFormula/timeturner.rb
@@ -0,0 +1,14 @@
+class Timeturner < Formula
+  version '1.2.0'
+  desc "Command line utility for manipulating date-time strings"
+  homepage "https://github.com/jrduncans/timeturner"
+
+  if OS.mac?
+      url "https://github.com/jrduncans/timeturner/releases/download/#{version}/timeturner-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "13877b966b192631816891cdba1955198f9f690f2c956c5e96cfcba6463f911a"
+  end
+
+  def install
+    bin.install "timeturner"
+  end
+end
