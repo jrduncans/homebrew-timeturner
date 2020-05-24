efb98c9235f8261ce159a5f2f51982aadc5d7341
Stephen Duncan <jrduncans@users.noreply.github.com>
2020-05-24 13:52:26 -0700

    Update timeturner to 1.3.0

--- HomebrewFormula/timeturner.rb
+++ HomebrewFormula/timeturner.rb
@@ -1,11 +1,11 @@
 class Timeturner < Formula
-  version '1.2.0'
+  version '1.3.0'
   desc "Command line utility for manipulating date-time strings"
   homepage "https://github.com/jrduncans/timeturner"
 
   if OS.mac?
-      url "https://github.com/jrduncans/timeturner/releases/download/v#{version}/timeturner-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "13877b966b192631816891cdba1955198f9f690f2c956c5e96cfcba6463f911a"
+      url "https://github.com/jrduncans/timeturner/releases/download/v#{version}/timeturner-v#{version}-x86_64-apple-darwin.zip"
+      sha256 "dc8dd5fc952fc1660f4f40b416250b981a779a2fb5b654641bdc468d333122b0"
   end
 
   def install
