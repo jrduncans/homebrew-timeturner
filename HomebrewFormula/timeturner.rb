7a69530c3b7b3e0bddfc6313fa3acef6b3f198df
Stephen Duncan <jrduncans@users.noreply.github.com>
2020-12-29 16:46:29 -0800

    Use universal release for now

--- HomebrewFormula/timeturner.rb
+++ HomebrewFormula/timeturner.rb
@@ -4,11 +4,8 @@ class Timeturner < Formula
   homepage "https://github.com/jrduncans/timeturner"
 
   if OS.mac?
-      url "https://github.com/jrduncans/timeturner/releases/download/v#{version}/timeturner-v#{version}-aarch64-apple-darwin.zip" => :arm64_big_sur
-      sha256 "4c6a8fde409e013cda9cc637ece751859405919a8ad6253329ea625a1648a8e3" => :arm64_big_sur
-
-      url "https://github.com/jrduncans/timeturner/releases/download/v#{version}/timeturner-v#{version}-x86_64-apple-darwin.zip"
-      sha256 "0864046bed52464be068c63525e652331185cbd8f9b40231ac6a3723051d135f"
+      url "https://github.com/jrduncans/timeturner/releases/download/v#{version}/timeturner-v#{version}-universal-apple-darwin.zip"
+      sha256 "2e3ef14ff50a6992446cfb488676827ae0815ac9275c4e4003b7856cb5a0746c"
   end
 
   def install
